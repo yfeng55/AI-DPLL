7b6dc80f8b027f7bd3bf2acfa93a5c4d0f11216f
Evan Feng <evanfeng55@gmail.com>
2016-10-09 13:20:23 -0400

    fixed backtracking state and deletion errors

--- src/com/yf833/Main.java
+++ src/com/yf833/Main.java
@@ -6,20 +6,24 @@ import java.util.*;
 
 public class Main {
 
-    public static ArrayList<String> clauses = new ArrayList<>();
-    public static HashSet<String> atoms = new HashSet<>();
+    public static ArrayList<String> initclauses = new ArrayList<>();
+    public static HashSet<String> initatoms = new HashSet<>();
 
     public static void main(String[] args) throws FileNotFoundException {
 
         // initialize clauses and atoms
         getInput(args[0]);
 
-//        System.out.println(clauses.toString());
-//        System.out.println(atoms.toString());
+        System.out.println("CLAUSES: ");
+        for(String s : initclauses){
+            System.out.println(s);
+        }
+        System.out.println("--------------------\nATOMS: ");
+        System.out.println(initatoms.toString());
+        System.out.println("--------------------\n");
 
         System.out.println();
-        System.out.println(DavisPutnam(clauses, atoms).toString() + "\n\n");
-
+        System.out.println(DavisPutnam(new ArrayList<>(initclauses), new HashSet<>(initatoms)).toString() + "\n\n");
     }
 
     // given a set of clauses and atoms;
@@ -31,13 +35,12 @@ public class Main {
         for(String atom : atoms){
             assignments.put(atom, null);
         }
-        for(String atom : atoms){
-            return DPL(atoms, clauses, assignments);
-        }
-        return null;
+
+        return DPL(atoms, clauses, assignments);
     }
 
 
+    // DPL METHOD (RECURSIVE)
     public static HashMap<String, Boolean> DPL(HashSet<String> atoms, ArrayList<String> clauses, HashMap<String, Boolean> assignments){
 
         // (1) Determine Success or Failure //
@@ -71,65 +74,72 @@ public class Main {
             else if (getSingleLiteral(clauses) != -1){
                 String L = clauses.get(getSingleLiteral(clauses));
                 obviousAssign(L, atoms, assignments);
-                propagate(L, clauses, assignments);
+                clauses = propagate(L, new ArrayList<>(clauses), new HashMap<>(assignments));
             }
             else{
                 break;
             }
         }
 
-        ///* PICK SOME ATOM AND TRY EACH ASSIGNMENT IN TURN */
-
-        // pick atom L such that V[L] == UNBOUND;  /* Try one assignment */
+        // PICK SOME ATOM AND TRY EACH ASSIGNMENT IN TURN //
+        // pick an unbound atom L
         String L = getNextUnassignedAtom(assignments);
         assignments.put(L, true);
         System.out.println("\n--> try " + L + " = true");
 
-        propagate(L, clauses, assignments);
-        HashMap<String, Boolean> newassignments = DPL(atoms, clauses, assignments);
+        ArrayList<String> clauses_T = propagate(L, new ArrayList<>(clauses), new HashMap<>(assignments));
+        HashMap<String, Boolean> newassignments = DPL(atoms, new ArrayList<>(clauses_T), new HashMap<>(assignments));
         if(newassignments != null){
             return newassignments;
         }
 
-        ///* IF V[A] := TRUE didn't work, try V[A} := FALSE;
+        // IF L = TRUE didn't work, try L = FALSE //
         assignments.put(L, false);
         System.out.println("--> try " + L + " = false");
-        propagate(L, clauses, assignments);
-        return DPL(atoms, clauses, assignments);
+        ArrayList<String> clauses_F = propagate(L, new ArrayList<>(clauses), new HashMap<>(assignments));
+        return DPL(atoms, new ArrayList<>(clauses_F), new HashMap<>(assignments));
 
     }
 
 
     // OBVIOUSASSIGN
     public static void obviousAssign(String L, HashSet<String> atoms, HashMap<String, Boolean> assignments){
-        if(atoms.contains(L)){
-            assignments.put(L, true);
-        }
-        else{
-            assignments.put(L, false);
-        }
+        if(atoms.contains(L)){ assignments.put(L, true); }
+        else{ assignments.put(L, false); }
     }
 
 
-    //PROPAGATE
-    public static HashMap<String, Boolean> propagate(String L, ArrayList<String> clauses, HashMap<String, Boolean> assignments){
+    //PROPAGATE: return a new set of clauses
+    public static ArrayList<String> propagate(String L, ArrayList<String> oldclauses, HashMap<String, Boolean> assignments){
+
+        ArrayList<String> newclauses = new ArrayList<>(oldclauses);
+        HashSet<Integer> items_to_remove = new HashSet<>();
 
         System.out.println("(propagate()): " + assignments.toString());
-        for(int i=0; i<clauses.size(); i++) {
-            if ((clauses.get(i).contains(" "+L) && assignments.get(L)==true) || (clauses.get(i).contains("-"+L) && assignments.get(L) == false)){
-                System.out.println("====> removing " + clauses.get(i));
-                clauses.remove(i);
+        System.out.println(newclauses.toString());
+
+        for(int i=0; i<newclauses.size(); i++) {
+            if ((newclauses.get(i).contains(" "+L) && assignments.get(L)==true) || (newclauses.get(i).contains("-"+L) && assignments.get(L) == false)){
+                System.out.println("====> removing clause " + newclauses.get(i));
+                items_to_remove.add(i);
             }
-            else if (clauses.get(i).contains(" "+L) && assignments.get(L) == false){
-                System.out.println("====> removing " + L + " from " + clauses.get(i));
-                clauses.set(i, removeFromClause(L, clauses.get(i)));
+            else if (newclauses.get(i).contains(" "+L) && assignments.get(L) == false){
+                System.out.println("====> removing " + L + " from " + newclauses.get(i));
+                newclauses.set(i, removeFromClause(L, newclauses.get(i)));
             }
-            else if (clauses.get(i).contains("-"+L) && assignments.get(L) == true){
-                System.out.println("====> removing " + "-"+L + " from " + clauses.get(i));
-                clauses.set(i, removeFromClause("-"+L, clauses.get(i)));
+            else if (newclauses.get(i).contains("-"+L) && assignments.get(L) == true){
+                System.out.println("====> removing " + "-"+L + " from " + newclauses.get(i));
+                newclauses.set(i, removeFromClause("-"+L, newclauses.get(i)));
             }
         }
-        return assignments;
+
+        //remove all clauses that need to be removed
+        for(int i=newclauses.size()-1; i>=0; i--){
+            if(items_to_remove.contains(i)){
+                newclauses.remove(i);
+            }
+        }
+        return newclauses;
     }
 
 
@@ -186,14 +196,14 @@ public class Main {
             if(nextclause.replaceAll("\\s+","").equals("0")){
                 break;
             }
-            clauses.add(nextclause);
+            initclauses.add(nextclause);
         }
 
         //initialize atoms (drop negative sign when comparing)
-        for(String clause : clauses){
+        for(String clause : initclauses){
             for(String atom : clause.split(" +")){
-                if(!atoms.contains(atom) && !atoms.contains(atom.substring((1)))){
-                    atoms.add(atom);
+                if(!initatoms.contains(atom) && !initatoms.contains(atom.substring((1)))){
+                    initatoms.add(atom);
                 }
             }
         }
